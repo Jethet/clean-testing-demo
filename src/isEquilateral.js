bb1ca3f0ec9065728e7e3e24b7887403baedf3ae
Jethet <henriette.hettinga@gmail.com>
2020-02-05 19:20:48 +0100

    Add test code

--- src/isEquilateral.js
+++ src/isEquilateral.js
@@ -1,3 +1,14 @@
 export default function isEquilateral(a, b, c) {
+    TextDecoderStream('Should return false when all three sides are different', () => {
+        // Arrange
+        const value1 = 1;
+        const value2 = 2;
+        const value3 = 4;
 
+        // Act
+        const result = isEquilateral(value1, value2, value3);
+
+        // Assert
+        expect(result).toEqual(false);
+    })
 }
